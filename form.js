9e159f26aa4a54d39d44e8e72ea3eaf42a15ede9
MelinaMedinaCa <94326325+MelinaMedinaCa@users.noreply.github.com>
2025-05-04 12:54:43 -0300

    Add files via upload

---
+++ form.js
@@ -0,0 +1,16 @@
+emailjs.init("xTMp9E14UrFGtT-sx");
+
+const form = document.getElementById("contacto");
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    emailjs.sendForm("service_kjn19fg", "template_ny1wly5, this")
+    .then(() => {
+        alert("MENSAJE ENVIADO CON EXITO.");
+        form.requestFullscreen();
+    })
+    .catch((error) => {
+        console.error("Error al enviar:", error);
+        alert("Ocurrio un error al enviar. Intentelo más Tarde.");
+    });
+});
